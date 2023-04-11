4cc65785da5d3163dbfc6f966f72099b74378256
Javok23 <130308615+Javok23@users.noreply.github.com>
2023-04-11 20:32:18 +0200

    Add files via upload

---
+++ src/main/java/jenkins_maven/mavenejerc6/Mavenejerc6Application.java
@@ -0,0 +1,13 @@
+package jenkins_maven.mavenejerc6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mavenejerc6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mavenejerc6Application.class, args);
+	}
+
+}
---
+++ src/test/java/jenkins_maven/mavenejerc6/Mavenejerc6ApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkins_maven.mavenejerc6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mavenejerc6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
